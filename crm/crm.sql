beb89211ceb475a40f58f053bbc94e33eade51bd
schud27 <dan.schulte@kellogg.northwestern.edu>
2022-01-20 02:23:35 +0000

    created Contacts, Salesperson, Activity, Companies

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,32 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+CREATE TABLE Contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT,
+    phone number TEXT
+);
+
+CREATE TABLE Salesperson (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    email TEXT
+);
+
+CREATE TABLE Activity (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    date_time TEXT,
+    notes TEXT
+    salesperson_id
+);
+
+CREATE TABLE Companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_name TEXT,
+    industry TEXT
+);
